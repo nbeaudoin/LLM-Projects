28590f23f412b0876758ef34bfdfbc4e89ea0dc6
Nicholas Beaudoin <nick.s.beaudoin@gmail.com>
2023-06-08 13:55:52 -0700

    updated app code

--- summarizerAI.py
+++ summarizerAI.py
@@ -12,14 +12,31 @@ st.header("Summarizer App using OpenAI + Streamlit")
 
 # Layout box 
 article_text = st.text_area("Enter your text you want to summarize")
-temp = st.slider("temperature", 0.0, 1.0, 0.5)
+output_size = st.radio(label="What kind of output do you want?",
+                       options=["To-the-point",
+                                "Concise",
+                                "Detailed"])
+
+if output_size == "To-the-point":
+    out_token = 50,
+elif output_size == "Concise":
+    out_token = 128
+else:
+    out_token = 516
 
 if len(article_text) > 100:
-    if st.button("Generate Summary"):
+    if st.button("Generate Summary", type="primary"):
         # Use GPT to generate a summary of the article
-        reponse = openai.Completion.create(
-            engine = "text-davinci-003",
+        response = openai.Completion.create(
+            engine = "text-davinci-002",
             prompt = "Please summarize this scientific article for me in a few seconds : " + article_text,
-            max_token = 516,
-            temperature = temp
+            max_tokens = out_token,
+            temperature = 0.5
         )
+
+        # Print the summary
+        res = response["choices"][0]["text"]
+        st.info(res)
+        st.download_button("Download Result", res)
+else:
+    st.warning("The sentence is not long enough!")
