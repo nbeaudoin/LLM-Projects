8eebe317f29314200b7cda438b85b08cde7840af
Nicholas Beaudoin <nick.s.beaudoin@gmail.com>
2023-06-14 16:14:08 -0700

    Updating requirements.txt

---
+++ langchain_starter_app.py
@@ -0,0 +1,6 @@
+# From tutorial: https://medium.com/databutton/getting-started-with-langchain-a-powerful-tool-for-working-with-large-language-models-286419ba0842
+
+# Dependencies to install 
+pip install streamlit 
+pip install langchain
+pip install openai
