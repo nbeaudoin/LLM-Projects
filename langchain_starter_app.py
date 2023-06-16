16e187d6e4980b15a293a8d09c5cbb42d11bde1a
Nicholas Beaudoin <nick.s.beaudoin@gmail.com>
2023-06-16 11:44:26 -0700

    Uploading langchain_starter_app

--- langchain_starter_app.py
+++ langchain_starter_app.py
@@ -28,3 +28,46 @@ user_question = st.text_input(
     "Enter Your Question : ",
     placeholder = "Cyanobacteria can perform photosynthetsis , are they considered as plants?",
 )
+
+# Generating the final answer to the user's question using all the chains
+if st.button("Tell me about it", type="primary"):
+    # Chain 1: Generating a rephrased version of the user's question
+    template = """{question}\n\n"""
+    prompt_template = PromptTemplate(input_variables=["question"], template=template)
+    question_chain = LLMChain(llm=llm, prompt=prompt_template)
+    st.header('Chain 1')
+    st.info(question_chain.run(user_question))
+#     # Chain 2: Generating assumptions made in the statement
+#     template = """Here is a statement:
+#         {statement}
+#         Make a bullet point list of the assumptions you made when producing the above statement.\n\n"""
+#     prompt_template = PromptTemplate(input_variables=["statement"], template=template)
+#     assumptions_chain = LLMChain(llm=llm, prompt=prompt_template)
+#     assumptions_chain_seq = SimpleSequentialChain(
+#         chains=[question_chain, assumptions_chain], verbose=True
+#     )
+
+#     # Chain 3: Fact checking the assumptions
+#     template = """Here is a bullet point list of assertions:
+#     {assertions}
+#     For each assertion, determine whether it is true or false. If it is false, explain why.\n\n"""
+#     prompt_template = PromptTemplate(input_variables=["assertions"], template=template)
+#     fact_checker_chain = LLMChain(llm=llm, prompt=prompt_template)
+#     fact_checker_chain_seq = SimpleSequentialChain(
+#         chains=[question_chain, assumptions_chain, fact_checker_chain], verbose=True
+#     )
+
+#     # Final Chain: Generating the final answer to the user's question based on the facts and assumptions
+#     template = """In light of the above facts, how would you answer the question '{}'""".format(
+#         user_question
+#     )
+#     template = """{facts}\n""" + template
+#     prompt_template = PromptTemplate(input_variables=["facts"], template=template)
+#     answer_chain = LLMChain(llm=llm, prompt=prompt_template)
+#     overall_chain = SimpleSequentialChain(
+#         chains=[question_chain, assumptions_chain, fact_checker_chain, answer_chain],
+#         verbose=True,
+#     )
+
+#     # Running all the chains on the user's question and displaying the final answer
+#     st.success(overall_chain.run(user_question))
